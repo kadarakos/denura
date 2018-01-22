1953472629c66d3d9eefbfd420c298e80925230d
Akos Kadar <u1257964@plum.uvt.nl>
2018-01-22 11:09:19 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+setup(name='denura',
+      version='0.1',
+      description='RNN Modules in Pytorch.',
+      url='https://github.com/kadarakos/denura',
+      author='Akos Kadar',
+      author_email='a.kadar@uvt.nl',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
+
